5f698fbdcd74bfb7a623f2b9a129b6432fb521a6
Britney Muller <britneymuller@gmail.com>
2019-02-28 16:54:16 -0800

    Updated comments
    Added my name and where I got this code from originally. #Egomaniac

--- get-image-urls.js
+++ get-image-urls.js
@@ -1,3 +1,4 @@
+// Stolen from Adrian Rosebrock's repo (https://www.pyimagesearch.com/2017/12/04/how-to-create-a-deep-learning-dataset-using-google-images/) & barely modified by Britney Muller.
 // pull down jquery into the JavaScript console
 var script = document.createElement('script');
 script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
@@ -8,7 +9,7 @@ document.getElementsByTagName('head')[0].appendChild(script);
 var urls = $('.rg_di .rg_meta').map(function() { return JSON.parse($(this).text()).ou; });
 
 
-// write the URls to a txt tile (one per line)
+// write the image URls to a txt tile (one per line)
 var textToSave = urls.toArray().join('\n');
 var hiddenElement = document.createElement('a');
 hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
